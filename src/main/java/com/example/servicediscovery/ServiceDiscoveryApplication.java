23f1fc33932ded3ee35d0de0d042064e0c621a30
clover <threeleafclover9@gmail.com>
2025-02-16 23:19:13 +0900

    package name

--- src/main/java/com/example/servicediscovery/ServiceDiscoveryApplication.java
+++ src/main/java/com/example/servicediscovery/ServiceDiscoveryApplication.java
@@ -1,4 +1,4 @@
-package com.example.serviceDiscovery;
+package com.example.servicediscovery;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- src/test/java/com/example/servicediscovery/ServiceDiscoveryApplicationTests.java
+++ src/test/java/com/example/servicediscovery/ServiceDiscoveryApplicationTests.java
@@ -1,4 +1,4 @@
-package com.example.serviceDiscovery;
+package com.example.servicediscovery;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
